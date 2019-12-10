ab04a099be7f43a38ce79c1aa30be19758da62d3
佳丽 <872868451@qq.com>
2019-12-10 18:16:14 +0800

    first commit

---
+++ src/com/kgc/pojo/Person.java
@@ -0,0 +1,13 @@
+package com.kgc.pojo;
+
+/**
+ * ClassName:Person
+ * Package:com.kgc.pojo
+ * Description:
+ *
+ * @date:2019/12/10 18:04
+ * @author:872868451@qq.com
+ */
+public class Person {
+    private String id;
+}
